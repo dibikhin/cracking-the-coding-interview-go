8b1b9cfd09bd401f2ab97f0a57f9ecbc096575ff
Roman Dibikhin <dibikhin@gmail.com>
2021-02-26 11:12:31 +0300

    urlify works, tested ok

---
+++ Arrays and Strings/arrsandstrs/1.3.URLify.go
@@ -0,0 +1,36 @@
+package arrsandstrs
+
+import (
+	"strings"
+)
+
+// Urlify replaces whitespaces w/ "%20" in place using []rune
+func Urlify(s string) string {
+	const p = "%20"
+
+	c := strings.Count(s, " ")
+	r := []rune(s)
+	rs := make([]rune, len(r)+c*2) // adding margin for shifting
+	copy(rs, r)
+
+	ix := index(rs, ' ')
+	for ix > -1 {
+		copy(rs[ix+2:], rs[ix:])
+
+		rs[ix] = '%'
+		rs[ix+1] = '2'
+		rs[ix+2] = '0'
+
+		ix = index(rs, ' ')
+	}
+	return string(rs)
+}
+
+func index(rs []rune, r rune) int {
+	for i, v := range rs {
+		if v == r {
+			return i
+		}
+	}
+	return -1
+}
--- Arrays and Strings/main.go
+++ Arrays and Strings/main.go
@@ -8,6 +8,9 @@ func main() {
 	println(IsAllCharsUniqA("abc"))  // true
 	println(IsAllCharsUniqB("日本本語")) // false
 
-	println(IsPermut("cab", "bac")) // true
+	println(IsPermut("cab", "bac"))  // true
 	println(IsPermut("xcvb", "bac")) // false
+
+	println(Urlify(" "))              // "%20"
+	println(Urlify(" côtier côtier")) // "%20côtier%20côtier"
 }
